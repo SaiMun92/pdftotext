a83e3392bca7b9240e24ebe10f4b8ca443a6838d
saimun <saimun92@gmail.com>
2019-07-24 11:04:35 +0800

    multiple subdirectory support

--- main.py
+++ main.py
@@ -38,25 +38,33 @@ def convert(fname, pages=None):
 
 #converts all pdfs in directory pdfDir, saves all resulting txt files to txtdir
 def convertMultiple(pdfDir, txtDir):
-    if pdfDir == "": pdfDir = os.getcwd() + "\\" #if no pdfDir passed in
-    for pdf in os.listdir(pdfDir): #iterate through pdfs in pdf directory
-        fileExtension = pdf.split(".")[-1]
-        if fileExtension == "pdf":
-            print("converting: " + pdf)
-            pdfFilename = pdfDir + "\\" + pdf
-            try:
-                text = convert(pdfFilename) #get string of text content of pdf
-            except Exception as e:
-                print(e)
-                continue
-            textFilename = txtDir + "\\" + pdf + ".txt"
-            print("writing to: " + textFilename)
-            textFile = open(textFilename, "w") #make text file
-            textFile.write(text) #write text to text file
+    if pdfDir == "":
+        pdfDir = os.getcwd() + "\\" #if no pdfDir passed in
+    for root, dirs, files in os.walk(pdfDir):
+        for pdf in files:
+            fileExtension = pdf.split(".")[-1]
+            filename = pdf.split(".")[0]
+            if fileExtension == "pdf":
+                print("converting: " + pdf)
+                pdfFilename = os.path.join(root, pdf)
+                try:
+                    text = convert(pdfFilename) #get string of text content of pdf
+                except Exception as e:
+                    print(e)
+                    continue
+                # textFilename = txtDir + "\\" + pdf + ".txt"
+                outroot = root.replace(pdfDir, txtDir)
+                textFilename = os.path.join(outroot, filename) + ".txt"
+                if not os.path.isdir(outroot):
+                    os.makedirs(outroot)
+
+                print("writing to: " + textFilename)
+                textFile = open(textFilename, "w") #make text file
+                textFile.write(text) #write text to text file
 
 
-pdfDir = r"F:\Development\pdf_downloader\combined"
-txtDir = r"F:\Development\pdf_downloader\combined_txt"
+pdfDir = r"C:\Users\sm.lee\Desktop\pdf"
+txtDir = r"C:\Users\sm.lee\Desktop\txt"
 convertMultiple(pdfDir, txtDir)
 
 
